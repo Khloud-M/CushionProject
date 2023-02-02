5ca4f807d2ff9d2ce898a92f4aeba9b701a76980
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-02 20:34:45 +0200

    add contect section in main

--- src/views/mainIndex.vue
+++ src/views/mainIndex.vue
@@ -2,12 +2,14 @@
  <div>
     
     <product-section/>
+    <contact-section/>
  </div>
 </template>
 <script>
+import ContactSection from './ContactSection.vue';
 import ProductSection from './ProductSection.vue';
 export default {
-  components: { ProductSection },
+  components: { ProductSection, ContactSection },
  
 };
 </script>
