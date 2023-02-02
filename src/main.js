fc0f14ca19355a3eaf249f5e17cad0188a404c79
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-02 20:15:38 +0200

    update store name

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import store from "./store";
+import store from "./stores";
 
 import '@/assets/style/main.css'
 import '@/assets/style/color.css'
