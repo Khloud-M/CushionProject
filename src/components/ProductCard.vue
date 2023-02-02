70f29d572a16ac4160b36130db2a1f7169c09397
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-02 14:48:32 +0200

    product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -6,7 +6,18 @@ export default {};
 <template>
   <div>
     <router-link to="/">
-    
+    <div class="image_wrapper">
+        <img src="" alt="">
+    </div>
+    <div class="info_wrapper">
+        <div class="product_name">
+            <h1></h1>
+        </div>
+        <div class="product_price">
+            <span class="price"></span>
+            <span class="currency">{{ $t('misc.USD') }}</span>
+        </div>
+    </div>
     </router-link>
   </div>
 </template>
