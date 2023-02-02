b85e70698a1f81e41f6a4d4d94bcc805caa1a580
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-03 01:07:20 +0200

    update card

---
+++ src/components/home/ProductSection.vue
@@ -0,0 +1,39 @@
+<script>
+import ProductCard from "@/components/ProductCard.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: { ProductCard },
+  computed: {
+    ...mapGetters({
+      products: "products",
+    }),
+  },
+};
+</script>
+<template>
+
+
+  <div>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          lg="4"
+          md="6"
+          v-for="product in products"
+          :key="product.id"
+         
+        >
+          <product-card :product="product"/>
+
+        
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+
+
+
+</template>
+
+<style></style>
