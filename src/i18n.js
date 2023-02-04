0617b43aa04864df9089d5117a0709e597ae625e
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-05 01:30:01 +0200

    change lang

--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,10 @@ import Cookies from "js-cookie";
 import en from "@/locales/en.json";
 import ar from "@/locales/ar.json";
 
-
 export default createI18n({
-  locale:Cookies.get('locale')|| 'en',
-  messages:{
-    en:en,
-    ar:ar
-  }
-})
+  locale: Cookies.get("locale") || "en",
+  messages: {
+    en: en,
+    ar: ar,
+  },
+});
