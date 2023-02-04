ba7b166dc1c3a580ef2843e773813cd8bddbefcf
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-02-04 14:37:15 +0200

    add aboutCollection Section

---
+++ src/components/home/abouutCollection.vue
@@ -0,0 +1,138 @@
+<template>
+  <section class="aboutCollection">
+    <v-container>
+      <v-row class="row1">
+        <v-col cols="12" lg="3" class="content">
+          <h3>BEGINNINGS</h3>
+          <p>
+            Credibly innovate granular internal or organic sources whereas high
+            standards in web-readiness. Energistically scale future-proof core
+            competencies vis-a-vis impactful experiences.
+          </p>
+          <p>
+            Dramatically synthesize integrated schemas. with optimal networks.
+          </p>
+        </v-col>
+        <v-col cols="12" lg="4" class="image">
+          <img
+            src="https://assets.website-files.com/5bd86c52b7abc5114b2ed43c/5bd8dbafef82dc4bd8245d41_nathan-fertig-249917-unsplash-p-1080.jpeg"
+            alt="image"
+          />
+        </v-col>
+      </v-row>
+      <hr />
+      <!-- end row1 -->
+      <v-row class="row2">
+        <v-col cols="12" lg="4" class="image">
+          <img
+            src="https://assets.website-files.com/5bd86c52b7abc5114b2ed43c/5bd8dbaf49a14257198e81cb_6.jpg"
+            alt="iamge"
+          />
+        </v-col>
+        <v-col cols="12" lg="3" class="content">
+          <h3>CARRER</h3>
+          <p>
+            Collaboratively administrate turnkey channels whereas virtual
+            e-tailers.
+          </p>
+          <p>
+            Objectively seize scalable metrics whereas proactive e-services
+            seamlessly empower fully researched growth strategies.
+          </p>
+          <p>And interoperable internal or “organic” sources.</p>
+          <button>learn more</button>
+        </v-col>
+      </v-row>
+      <hr />
+
+      <!-- end row2 -->
+
+      <v-row class="row3">
+        <v-col cols="12" lg="3" class="content">
+          <h3>PRODUCTS</h3>
+          <p>
+            Seamlessly empower fully researched growth strategies and
+            interoperable internal or “organic” sources.
+          </p>
+          <p>
+            Credibly innovate granular internal or “organic” sources whereas
+            high standards in web-readiness.
+          </p>
+          <p>Energistically scale future-proof core competence</p>
+        </v-col>
+        <v-col cols="12" lg="4" class="image">
+          <img
+            src="https://assets.website-files.com/5bd86c52b7abc5114b2ed43c/5bd8dbafcf26d38b797ab7a8_julian-hochgesang-795082-unsplash.jpg"
+            alt="image"
+          />
+        </v-col>
+      </v-row>
+      <!-- end row1 -->
+    </v-container>
+  </section>
+</template>
+<style lang="scss" scoped>
+.aboutCollection {
+  margin: var(--margin-topbotton) 0;
+  hr{
+    width: 65%;
+    margin: auto;
+    border: 0.5px solid var(--main-color);
+  }
+  .v-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 50px;
+    flex-wrap: wrap;
+    height: 55vh;
+    margin: var(--margin-topbotton) 0;
+  }
+  .content {
+    height: 35vh;
+    margin: auto 0;
+    line-height: 40px;
+    button {
+      border: 1px solid var(--main-color);
+      border-radius: 20px;
+      padding: 5px 20px;
+      width: 180px;
+      margin: 10px auto;
+      text-transform: uppercase;
+      color: var(--main-color);
+      font-weight: 700;
+      font-size: 13px;
+      letter-spacing: 2px;
+      transition: all 0.3s ease;
+      &:hover {
+        background-color: var(--main-color);
+        color: #fff;
+      }
+    }
+
+    h3 {
+      color: var(--main-color);
+    }
+    p {
+      color: var(--second-color);
+      font-size: 20px;
+    }
+  }
+  .image {
+    height: 55vh;
+    border: 17px solid var(--main-color);
+    margin: 0 !important;
+    padding: 0 !important;
+    cursor: pointer;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      vertical-align: middle;
+      display: inline-block;
+      margin: 0 !important;
+      padding: 0 !important;
+    }
+  }
+}
+</style>
--- src/views/mainIndex.vue
+++ src/views/mainIndex.vue
@@ -3,6 +3,7 @@
  <div>
  <elementPage />
  <AboutSection/>
+ <abouutCollection/>
     <product-section/>
     <contact-section/>
     <newsletters-section/>
@@ -14,8 +15,9 @@ import ProductSection from '@/components/home/ProductSection.vue';
 import elementPage from "@/components/home/elementPage.vue";
 import NewslettersSection from '@/components/home/NewslettersSection.vue';
 import AboutSection from '../components/home/aboutSection.vue';
+import abouutCollection from '../components/home/abouutCollection.vue';
 export default {
-  components: { ProductSection, elementPage, AboutSection, NewslettersSection, ContactSection },
+  components: { ProductSection, elementPage, AboutSection, abouutCollection, NewslettersSection, ContactSection },
  
 
 
