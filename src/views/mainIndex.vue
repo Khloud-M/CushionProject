f8a04f07c42422465606331e3aa7e5cd8f8245dc
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-04 22:56:41 +0200

    add benefit in main section

--- src/views/mainIndex.vue
+++ src/views/mainIndex.vue
@@ -1,28 +1,32 @@
 <template>
-
- <div>
- <elementPage />
- <AboutSection/>
- <abouutCollection/>
-    <product-section/>
-    <contact-section/>
-    <newsletters-section/>
- </div>
+  <div>
+    <elementPage />
+    <AboutSection />
+    <abouutCollection />
+    <product-section />
+    <contact-section />
+    <newsletters-section />
+    <benifits-section />
+  </div>
 </template>
 <script>
-import ContactSection from '@/components/home/ContactSection.vue';
-import ProductSection from '@/components/home/ProductSection.vue';
+import ContactSection from "@/components/home/ContactSection.vue";
+import ProductSection from "@/components/home/ProductSection.vue";
 import elementPage from "@/components/home/elementPage.vue";
-import NewslettersSection from '@/components/home/NewslettersSection.vue';
-import AboutSection from '../components/home/aboutSection.vue';
-import abouutCollection from '../components/home/abouutCollection.vue';
+import NewslettersSection from "@/components/home/NewslettersSection.vue";
+import AboutSection from "../components/home/aboutSection.vue";
+import abouutCollection from "../components/home/abouutCollection.vue";
+import BenifitsSection from "../components/home/BenifitsSection.vue";
 export default {
-  components: { ProductSection, elementPage, AboutSection, abouutCollection, NewslettersSection, ContactSection },
- 
-
-
-
+  components: {
+    ProductSection,
+    elementPage,
+    AboutSection,
+    abouutCollection,
+    NewslettersSection,
+    ContactSection,
+    BenifitsSection,
+  },
 };
 </script>
-<style>
-</style>
+<style></style>
