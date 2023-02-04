e77707382122bf6e9b2e94111ea7daca7a3cc2c3
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-05 00:27:56 +0200

    add footer in main

--- src/views/mainIndex.vue
+++ src/views/mainIndex.vue
@@ -7,6 +7,7 @@
     <contact-section />
     <newsletters-section />
     <benifits-section />
+    <footer-section/>
   </div>
 </template>
 <script>
@@ -17,6 +18,7 @@ import NewslettersSection from "@/components/home/NewslettersSection.vue";
 import AboutSection from "../components/home/aboutSection.vue";
 import abouutCollection from "../components/home/abouutCollection.vue";
 import BenifitsSection from "../components/home/BenifitsSection.vue";
+import FooterSection from '../components/home/FooterSection.vue';
 export default {
   components: {
     ProductSection,
@@ -26,6 +28,7 @@ export default {
     NewslettersSection,
     ContactSection,
     BenifitsSection,
+    FooterSection,
   },
 };
 </script>
