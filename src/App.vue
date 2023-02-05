916ca2877ba6d4c27cabfa8466de4c62a7375e6a
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-02-06 01:23:32 +0200

    add transition on view-router

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,38 @@
   <div>
     <labHeader />
 
-    <router-view />
-    <benifits-section/>
-    <footer-section/>
+    <router-view
+      data-aos="fade-in"
+      data-aos-offset="200"
+      data-aos-delay="100"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="true"
+      data-aos-anchor-placement="top-center"
+    />
+    <benifits-section />
+    <footer-section />
   </div>
 </template>
 
 <script>
-import labHeader from "@/components/layout/labHeader.vue"
-import BenifitsSection from './components/home/BenifitsSection.vue'
-import FooterSection from './components/home/FooterSection.vue'
+import labHeader from "@/components/layout/labHeader.vue";
+import BenifitsSection from "./components/home/BenifitsSection.vue";
+import FooterSection from "./components/home/FooterSection.vue";
 
 export default {
-  components:{
+  components: {
     labHeader,
     BenifitsSection,
     FooterSection,
-    
-  }
-}
+  },
+};
 </script>
-<style >
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
 </style>
